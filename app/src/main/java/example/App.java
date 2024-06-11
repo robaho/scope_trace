53542c00dfcf408875ee1f87639e9504a62a6503
robert engels <robaho@users.noreply.github.com>
2024-06-11 09:58:29 -0500

    test scoped tracing

---
+++ app/src/main/java/example/App.java
@@ -0,0 +1,33 @@
+package example;
+
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.LockSupport;
+
+public class App {
+    private static final Random RANDOM = new Random();
+    public void callServer() {
+        // the try-with-resources would be emitted by byte code injection
+        try(final var timer = TraceContext.get().time("callServer")) {
+            LockSupport.parkNanos(TimeUnit.MILLISECONDS.toNanos(RANDOM.nextInt(100)));
+        }
+    }
+
+    public void handleRequest() {
+        try(final var timer = TraceContext.get().time("handleRequest")) {
+            callServer();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        App app = new App();
+        ExecutorService server = Executors.newVirtualThreadPerTaskExecutor();
+        for(int i=0;i<1000;i++) {
+            // this is the only change required to setup the context at the start of a request
+            server.execute(TraceContext.traceRequest(i,() ->  app.handleRequest()));
+        }
+        server.awaitTermination(30, TimeUnit.SECONDS);
+    }
+}
---
+++ app/src/main/java/example/TraceContext.java
@@ -0,0 +1,45 @@
+package example;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class TraceContext {
+    private static final ScopedValue<TraceContext> context = ScopedValue.newInstance();
+    
+    private final List<Event> events = new LinkedList();
+    private final long requestId;
+
+    private TraceContext(long requestId) {
+        this.requestId = requestId;
+    }
+
+    public Timer time(String method) {
+        return new Timer(method);
+    }
+    public class Timer implements AutoCloseable {
+        private final long start = System.currentTimeMillis();
+        private final String method;
+        private Timer(String method) {
+            this.method = method;
+        }
+        @Override
+        public void close() {
+            events.add(new Event(start,System.currentTimeMillis(),"method "+method));
+        }
+    }
+    public record Event(long start,long end,String description){}
+    public void dump() {
+        events.stream().forEach(e -> System.out.println("request "+requestId+", event "+e+", duration "+(e.end-e.start)+"ms"));
+    }
+    public static TraceContext get() {
+        return context.get();
+    }
+    public static Runnable traceRequest(long requestId,final Runnable r) {
+        return () -> {
+            ScopedValue.runWhere(context,new TraceContext(requestId),() -> {
+                r.run();
+                TraceContext.get().dump();
+            });
+        };
+    }
+}
